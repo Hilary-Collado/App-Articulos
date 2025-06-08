bf9ede95a7511dcc0b8d0460b3eeda3378c62213
Hilary-Collado <hilarycollado1@gmail.com>
2025-06-08 16:02:41 -0300

    Creando app movil con react navite. Consumiendo API de articulos.

--- App.js
+++ App.js
@@ -1,20 +1,99 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from "react";
+import { StatusBar } from "expo-status-bar";
+import {
+  FlatList,
+  StyleSheet,
+  Text,
+  View,
+  ActivityIndicator,
+} from "react-native";
+import { use } from "react";
 
-export default function App() {
+const App = () => {
+  const [productos, setProductos] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetch("https://softecard.com/borrar.php?t=Articulo_Lista_Select&consulta=")
+      .then((res) => res.json())
+      .then((data) => {
+        console.log("Productos cargados:", data);
+        setProductos(data.articulos);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.error("Error al cargar los productos:", error);
+        setLoading(false);
+      });
+  }, []);
+
+  const renderItem = ({ item }) => {
+    return (
+      <View style={styles.card}>
+        <Text style={styles.titulo}>{item.articulo}</Text>
+        <Text>Marca: {item.marca}</Text>
+        <Text>Precio: RD${item.precio}</Text>
+        <Text>Existencia: {item.existencia}</Text>
+        <Text>Ubicación: {item.ubicacion}</Text>
+      </View>
+    );
+  };
+
+  if (loading) {
+    return (
+      <View style={styles.centered}>
+        <ActivityIndicator size="large" color="#007bff" />
+        <Text>Cargando productos...</Text>
+      </View>
+    );
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <FlatList
+        data={productos}
+        keyExtractor={(item) => item.id_articulo.toString()}
+        renderItem={renderItem}
+        contentContainerStyle={styles.lista}
+      />
+      <Text>Hola mundo</Text>
+      {/* <StatusBar style="auto" /> */}
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    // alignItems: 'center',
+    // justifyContent: 'center',
+    paddingTop: 40,
+  },
+  lista: {
+    paddingHorizontal: 16,
+    paddingBottom: 20,
+  },
+  card: {
+    backgroundColor: "#fff",
+    padding: 16,
+    marginBottom: 12,
+    borderRadius: 12,
+    shadowColor: "#000",
+    shadowOpacity: 0.1,
+    shadowRadius: 4,
+    elevation: 3,
+  },
+  titulo: {
+    fontSize: 16,
+    fontWeight: "bold",
+    marginBottom: 6,
+  },
+  centered: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
   },
 });
+
+export default App;
